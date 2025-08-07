0af37133635eba22d11aabcc914039c461584050
Balaji0502 <161752065+Balaji0502@users.noreply.github.com>
2025-08-07 21:34:12 +0530

    Create Possitive_neg_check.java

---
+++ Possitive_neg_check.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        int num = 0;
+        if(num>=0){
+            if(num==0){
+                System.out.println("Zero");
+            }
+            else
+            System.out.println("The number is positive");
+            
+        }
+        
+        else{
+            System.out.println("Zero");
+        }
+    }
+}
